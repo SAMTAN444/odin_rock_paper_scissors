89e1f4a3f5c548024b04c9de09b83e55eb473756
Samuel Tan <notsamueltjy@gmail.com>
2023-12-22 13:16:08 +0800

    Finished up styles.css and added functions to script.js

---
+++ script.js
@@ -0,0 +1,123 @@
+// GAME
+
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ''
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie'
+    }
+    if (
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') || 
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK') || 
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')
+    ) {
+        playerScore++
+        roundWinner = 'player'
+    }
+    if (
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') || 
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK') || 
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')
+    ) {
+        computerScore++
+        roundWinner = 'computer'
+    }
+    updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
+
+function getRandomChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+        case 0:
+            return 'ROCK'
+        case 1:
+            return 'PAPER'
+        case 2:
+            return 'SCISSORS'
+    }
+}
+
+function isGameOver() {
+    return playerScore === 5 || computerScore === 5
+}
+
+//UI
+
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+const playerScorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+const rockBtn = document.getElementById('rockBtn')
+const paperBtn = document.getElementById('paperBtn')
+const scissorsBtn = document.getElementById('scissorsBtn')
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('endgameMsg')
+const overlay = document.getElementById('overlay')
+const restartBtn = document.getElementById('restartBtn')
+
+rockBtn.addEventListener('click', () => handleClick('ROCK'))
+paperBtn.addEventListener('click', () => handleClick('PAPER'))
+scissorsBtn.addEventListener('click', () => handleClick('SCISSORS'))
+restartBtn.addEventListener('click', restartGame)
+overlay.addEventListener('click', closeEndgameModal)
+
+function handleClick(playerSelection) {
+    if  (isGameOver()) {
+        openEndgameModal()
+        return
+    } 
+
+    const computerSelection = getRandomChoice()
+    playRound(playerSelection, computerSelection)
+    updateChoices(playerSelection, computerSelection)
+    updateScore() 
+
+    if(isGameOver()) {
+        openEndgameModa()
+        setFinalMessage()
+    }
+}
+
+function updateChoices(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case 'ROCK':
+            playerSign.textContent = '✊'
+            break
+        case 'PAPER':
+            playerSign.textContent = '✋'
+            break
+        case 'SCISSORS':
+            playerSign.textContent = '✌️'
+            break
+    }
+
+    switch (computerselection) {
+        case 'ROCK':
+            computerSign.textContent = '✊'
+            break
+        case 'PAPER':
+            compterSign.textContent = '✋'
+            break
+        case 'SCISSORS':
+            compterSign.textContent = '✌️'
+            break
+
+    }
+}
+
+function updateScore() {
+    if(roundWinner === 'tie') {
+        scoreInfo.textContent = "It's a tie!"
+    } else if (roundWinner === 'player') {
+        scoreInfo.textContent = "You won!"
+    } else if (roundWinner === 'computer') {
+        scoreInfo.textContent = "You lost!"
+    }
+
+    playerScorePara.textContent = `Player: ${playerScore}`
+    computerScorePara.textContent = `Computer: ${computerScore}`
+}
